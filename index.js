cb094939ca63072ad06c8529b84f87d3446df925
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-01-15 20:24:00 +0000

    spaced out

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 let counter = 0;
-let prefixIncrement = ++counter;
-let postfixIncrement = counter++;
-let prefixDecrement = --counter;
-let postfixDecrement = counter--;
-document.getElementById("result").innerHTML - counter;
+ prefixIncrement = + + counter;
+ postfixIncrement = counter + +;
+ prefixDecrement = - - counter;
+ postfixDecrement = counter - -;
+document.getElementById("result").innerHTML = counter;
 
+
+
+//    ✕ should assign the prefix incremented value of counter to a variable named prefixIncrement (130 ms)// 
+//    ✕ should assign the postfix incremented value of counter to a variable named postfixIncrement (137 ms)
+//    ✕ should assign the prefix decrement value of counter to a variable named prefixDecrement (132 ms)
+//    ✕ should assign the postfix decrement value of counter to a variable named postfixDecrement (131 ms)
+//    ✕ should assign the innerHTML of the HTML element with the id result to the counter (136 ms)
