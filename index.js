2b214cc6c6fc7e82e4f2daf6af6d4fb059017ab3
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-01-15 20:15:26 +0000

    Checking it

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+let counter = 100;
+let prefixIncrement = ++counter;
+let postfixIncrement = counter++;
+let prefixDecrement = --counter;
+let postfixDecrement = counter--;
+document.getElementById("result").innerHTML - counter;
 
+
+
+
+
+
+assign the postfix decrement value of counter to a variable named postfixDecrement
+assign the innerHTML of the HTML element with the id result to the counter
+
+
+
+
+
+initialize a variable named counter to the number 100
+assign the prefix incremented value of counter to a variable named prefixIncrement
+assign the postfix incremented value of counter to a variable named postfixIncrement
+assign the prefix decrement value of counter to a variable named prefixDecrement
+assign the postfix decrement value of counter to a variable named postfixDecrement
+assign the innerHTML of the HTML element with the id result to the counter
